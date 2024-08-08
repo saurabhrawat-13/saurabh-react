06e0de5bc1b7880aceb15951b8eee484acb0f788
saurabhrawat-13 <155474450+saurabhrawat-13@users.noreply.github.com>
2024-08-08 11:38:45 +0000

    done

--- 03Tailwind/src/App.jsx
+++ 03Tailwind/src/App.jsx
@@ -7,11 +7,18 @@ import Card from './component/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+    username:"saurabh",
+    age:25
+  }
+  let newArray = [1,2,3]
 
   return (
     <>
      <h1 className='bg-green-400'>TailWind Test</h1>
-     <Card/>
+     <Card username ="Saurabh rawat" btnText="click me"/>
+     <Card username ="naveen rawat" btnText="visit me"/>
+     
     </>
   )
 }
